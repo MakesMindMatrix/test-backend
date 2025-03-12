265a87188ace56a89e4aeb89a09bd3ec40cf28a7
Nimish Kumar <131428877+MakesMindMatrix@users.noreply.github.com>
2025-03-12 11:20:43 +0530

    test yml

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,7 +4,7 @@ exports.testRoute = (async (_req, res, next) => {
     try {
         res.status(201).json({
             success: true,
-            message: "Route working succesfully",
+            message: "Route working succesfully test",
         });
     } catch (error) {
         console.log(error)
