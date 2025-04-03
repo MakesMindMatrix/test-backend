d261bf9d1e333993e35845f3a5473b829a4474bd
Nimish Kumar <131428877+MakesMindMatrix@users.noreply.github.com>
2025-04-03 16:34:58 +0530

    test

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,7 +4,7 @@ exports.testRoute = (async (_req, res, next) => {
     try {
         res.status(201).json({
             success: true,
-            message: "Route working succesfully test",
+            message: "Route working succesfully test - 2",
         });
     } catch (error) {
         console.log(error)
